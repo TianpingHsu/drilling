fe075e5441fea6d81c9940fe77afd180eb94244e
EvergreenHZ <huaizhixtp@163.com>
2023-07-23 14:58:44 +0800

    largestNumber

--- solu.h
+++ solu.h
@@ -3,45 +3,81 @@
 #include "inc/utils.h"
 
 class Solution {
-public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        multiset<int> s;
-        priority_queue<int> q;
-        vector<int> ret;
+    static bool is_greater(const string &a, const string& b) {
+        //show(vector<string>{a, b});
+        if (a.size() == b.size()) return a > b;
         int i = 0;
-        while (i < k - 1) {
-            s.insert(nums[i]);
+        while (i < a.size() && i < b.size()) {
+            if (a[i] != b[i]) return a[i] > b[i];
             i++;
         }
-        for (; i < nums.size(); i++) {
-            s.insert(nums[i]);
-            show(s);
-            ret.push_back(*s.rbegin());
-            s.erase(s.find(nums[i - k + 1]));
+        if (i == a.size() && i < b.size()) {
+            return is_greater(a, b.substr(a.size(), b.size()));
+        } 
+        if (i == b.size() && i < a.size()) {
+            return is_greater(a.substr(b.size(), a.size()), b);
         }
-#ifdef TEST
-        PRINT_VEC(ret);
-#endif
+        return false;
+    }
+
+    bool all0(const string &s) {
+        for (auto x : s) if (x != '0') return false;
+        return true;
+    }
+
+public:
+    string largestNumber(vector<int>& nums) {
+        string ret;
+        vector<string> v;
+        for (auto x : nums) {
+            v.push_back(to_string(x));
+        }
+        sort(v.begin(), v.end(), is_greater);
+        for (auto x : v) ret += x;
+        if (all0(ret)) return "0";
         return ret;
     }
 
 #ifdef TEST
     void test() {
-#if 0
         {
-            vector<int> v = createVector("[1,3,-1,-3,5,3,6,7]");
-            vector<int> ans{3,3,5,5,6,7};
-            CHECK((maxSlidingWindow(v, 3) == ans), true);
-        }
-#endif
-        {
-            vector<int> v = createVector("[-7,-8,7,5,7,1,6,0]");
-            show(v);
-            vector<int> ans{7,7,7,7,7};
-            CHECK((maxSlidingWindow(v, 4) == ans), true);
+            //auto v = createVector("[10,2]");
+            //std::string ret = largestNumber(v);
+            //CHECK(ret, "210");
         }
 
+        {
+            //auto v = createVector("[3,30,34,5,9]");
+            //std::string ret = largestNumber(v);
+            //CHECK(ret, "9534330");
+        }
+        {
+            auto v = createVector("[999999991,9]");
+            std::string ret = largestNumber(v);
+            CHECK(ret, "9999999991");
+        }
     }
 #endif
-
 };
+
+/*
+ class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string> ss;
+        bool all0 = true;
+        for (auto x : nums) {
+            ss.push_back(std::to_string(x));
+            if (x != 0) all0 = false;
+        }
+        if (all0) return "0";
+        std::sort(ss.begin(), ss.end(), [](const string &s1, const string &s2) {
+            if (s1.size() == s2.size()) return s1 > s2;
+            return s1 + s2 > s2 + s1;
+        });
+        string ret;
+        for (auto x : ss) ret += x;
+        return ret;
+    }
+};
+ */
--- src/lc/179.h
+++ src/lc/179.h
@@ -1,6 +1,67 @@
-#include "utils.h"
+
+
+#include "inc/utils.h"
 
 class Solution {
+    static bool is_greater(const string &a, const string& b) {
+        //show(vector<string>{a, b});
+        if (a.size() == b.size()) return a > b;
+        int i = 0;
+        while (i < a.size() && i < b.size()) {
+            if (a[i] != b[i]) return a[i] > b[i];
+            i++;
+        }
+        if (i == a.size() && i < b.size()) {
+            return is_greater(a, b.substr(a.size(), b.size()));
+        } 
+        if (i == b.size() && i < a.size()) {
+            return is_greater(a.substr(b.size(), a.size()), b);
+        }
+        return false;
+    }
+
+    bool all0(const string &s) {
+        for (auto x : s) if (x != '0') return false;
+        return true;
+    }
+
+public:
+    string largestNumber(vector<int>& nums) {
+        string ret;
+        vector<string> v;
+        for (auto x : nums) {
+            v.push_back(to_string(x));
+        }
+        sort(v.begin(), v.end(), is_greater);
+        for (auto x : v) ret += x;
+        if (all0(ret)) return "0";
+        return ret;
+    }
+
+#ifdef TEST
+    void test() {
+        {
+            //auto v = createVector("[10,2]");
+            //std::string ret = largestNumber(v);
+            //CHECK(ret, "210");
+        }
+
+        {
+            //auto v = createVector("[3,30,34,5,9]");
+            //std::string ret = largestNumber(v);
+            //CHECK(ret, "9534330");
+        }
+        {
+            auto v = createVector("[999999991,9]");
+            std::string ret = largestNumber(v);
+            CHECK(ret, "9999999991");
+        }
+    }
+#endif
+};
+
+/*
+ class Solution {
 public:
     string largestNumber(vector<int>& nums) {
         vector<string> ss;
@@ -19,3 +80,4 @@ public:
         return ret;
     }
 };
+ */
